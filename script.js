8b9ce2bfe7cdc8824205f7d7cda0173a5f7d508d
rodelul <98654686+rodelul@users.noreply.github.com>
2024-08-27 18:50:03 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const gridList = document.querySelector('hero-grid');
+const gridLi = document.querySelector('.hero-grid');
 
-fetch('data.json')
+fetch('./data.json')
     .then(res => res.json())
     .then(data => {
-        data.forEach(product => {
-            gridList.insertAdjacentHTML('beforeend', `<h1>pret ${data.name}</h1>`);
-        })
+       data.forEach(product => {
+        const card = `
+            <div class="product">
+                    <img src="${product.image.desktop}" />
+                     <button>Add to Cart</button>
+                    <h2>${product.category}</h2>
+                    <h1>${product.name}</h1>
+                    <p>${product.price}</p>
+                   
+            </div>
+        `;
+        gridLi.innerHTML += card;
+
+       })
     })
+
+
